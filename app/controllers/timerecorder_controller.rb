b9e9ffa28ad34043fea17d3ad3bae9a5690e9ce9
Kevin Butchart <kevinbutchart@ownmail.net>
2014-08-18 00:16:57 +0100

    time zone updates

--- app/controllers/timerecorder_controller.rb
+++ app/controllers/timerecorder_controller.rb
@@ -6,8 +6,8 @@ class TimerecorderController < ApplicationController
 	def signin
 		tr = TimeRecord3.new
 		currtime = Time.now.localtime
-		t = currtime.to_time
-		t = time_to_next_quarter_hour(t)
+		t = currtime.in_time_zone
+		# t = time_to_next_quarter_hour(t)
 		tr.time = currtime
 		tr.recordtype = "in"
 	
@@ -16,14 +16,14 @@ class TimerecorderController < ApplicationController
 		currdate = currtime.to_date
 		dr = DayRecord3.find_by logdate: currdate
 		if (dr) 
-			dr.pmintime = t
+			dr.pmintime = t.in_time_zone
 		else
 			dr = DayRecord3.new
 			dr.logdate = currdate
-			dr.amintime = t
+			dr.amintime = t.in_time_zone
 			dr.amouttime = Time.local(Time.now.year, Time.now.month, Time.now.day, 12,0) 
 			dr.pmintime = Time.local(Time.now.year, Time.now.month, Time.now.day, 13,0) 
-			dr.pmouttime = t
+			dr.pmouttime = t.in_time_zone
 		end
 		dr.save
 
